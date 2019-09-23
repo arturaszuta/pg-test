3506e1a65bfa8c226231494f113d9c1af61631e1
Arturas Zuta <arturaszuta@gmail.com>
2019-09-23 15:22:14 -0400

    initial setup

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const { postgraphile } = require('postgraphile')
+
+const app = express();
+
+app.use(
+  postgraphile(process.env.DATABASE_URL,
+    "public",
+    {
+      watchPg: true,
+      graphiql: true,
+      enhanceGraphiql: true,
+    }
+    )
+  );
+
+  app.linsten(process.env.port);
